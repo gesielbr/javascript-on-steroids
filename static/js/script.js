25b0136fd2f16ecc0f6f9f456baac3fb0425b998
Gesiel Souza Oliveira <gesiel.br@gmail.com>
2021-05-13 18:46:27 -0300

    Botões e funções para inserir conteúdo e apagar conteúdo

--- static/js/script.js
+++ static/js/script.js
@@ -1,3 +1,15 @@
 function ageInDays() {
   var birthYear = prompt("Que ano você nasceu?");
+  var ageInDays = (2021 - birthYear) * 365;
+  var h1 = document.createElement("h1");
+  var textAnswer = document.createTextNode(
+    "Você tem " + ageInDays + " dias de vida."
+  );
+  h1.setAttribute("id", "ageInDays");
+  h1.appendChild(textAnswer);
+  document.getElementById("flex-box-result").appendChild(h1);
+}
+
+function reset() {
+  document.getElementById("ageInDays").remove();
 }
